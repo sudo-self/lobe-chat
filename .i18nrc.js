505bcb229e24e86f268569b7f7eda250e56c85d8
sudo-self <sudo-self@proton.me>
2023-12-14 08:24:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ .i18nrc.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('@lobehub/i18n-cli');
+
+module.exports = defineConfig({
+  entry: 'locales/zh_CN',
+  entryLocale: 'zh_CN',
+  output: 'locales',
+  outputLocales: ['zh_TW', 'en_US', 'ru_RU', 'ja_JP', 'ko_KR', 'fr_FR'],
+  temperature: 0,
+  modelName: 'gpt-3.5-turbo-1106',
+  splitToken: 1024,
+  experimental: {
+    jsonMode: true,
+  },
+  markdown: {
+    entry: ['./README.md'],
+    outputLocales: ['zh_CN'],
+    outputExtensions: (locale) => {
+      if (locale === 'en_US') return '.md';
+      return `.${locale.replace('_', '-')}.md`;
+    },
+  },
+});
